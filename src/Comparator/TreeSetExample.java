9f5431c1a33923ee7917ce0c7b0d0821021b9bc6
Touyer <touyer.lees@gmail.com>
2024-12-11 23:30:47 -0600

    Initial commit

---
+++ src/Comparator/TreeSetExample.java
@@ -0,0 +1,4 @@
+package Comparator;
+
+public class TreeSetExample {
+}
---
+++ src/Comparator/cities_Comparator.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+public class cities_Comparator implements Comparator<String> {//Comparator is a interface that allow custom sorting
+    public static void main(String[] args) {
+//Output 2
+    }
+}
